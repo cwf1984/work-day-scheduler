3fab81ac85a3e8ec818a605128bd6410fde6643c
Vanessa Malloy <catwoman.fan.84@gmail.com>
2020-11-06 22:59:39 -0800

    color coding, local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,15 @@
 // GIVEN I am using a daily planner to create a schedule
+// THIS r
+$(document).ready(function(){
+$(".saveBtn").on("click", function(){
+    var clickEvent = $(this).siblings("#textInput").val();
+    console.log(clickEvent);
+    var parentId = $(this).parent().attr("id");
+    console.log(parentId); 
+    localStorage.setItem(clickEvent, parentId); 
+    localStorage.getItem(clickEvent);
+})
+
 
 
 // WHEN I open the planner
@@ -8,29 +19,27 @@ var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 
 var dayHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-var textInput = $("#textInput");
+console.log(currentDay);
+var description = $(".description");
 
 for (var i = 0; i < dayHours.length; i++) {
     var currentHourOne = parseInt(currentDay.format("H"));
-    var currentHourTwo = currentDay.hour();
+    var currentHourTwo = currentDay.hours();
     console.log(currentHourOne, currentHourTwo);
     //textarea color coded as currentHourTwo
 
-    if (currentDay < dayHours) {
-    textInput.attr("class", "past");
+    if (currentHourOne < currentHourTwo) {
+    description.attr("class", "past");
     }
 
-    else if (currentDay == dayHours) {
-    textInput.attr("class", "present");
+    else if (currentHourOne == currentHourTwo) {
+    description.attr("class", "present");
     }
 
     else {
-    textInput.attr("class", "future");
+    description.attr("class", "future");
     }
 };
-
-// var currentHour = moment().format("LT");
-// console.log(currentHour);
 
 
 // WHEN I view the timeblocks for that day
@@ -38,55 +47,15 @@ for (var i = 0; i < dayHours.length; i++) {
 
 //code checks current hour to determine if past, present or future and apply appropriate style from css -- modify class name to set state to modify colors
 
-// function timeChange() {
-//     for (var i=0; i < .length; i++) {
-
-
-
-// timeChange;
-// console.log(timeChange);
-// }}};
-
 // WHEN I click into a timeblock
 // THEN I can enter an event
 
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 
-function storeLocalStorage() {
-
-var userInput = localStorage.getItemById("textInput", JSON.stringify(userInput));
-
-textInput.textContent=userInput;
-
-
-var userInput = localStorage.getItemById("textInput")
-var saveBtn = document.querySelector("#saveBtn");
-
-saveBtn.addEventListener("click", function(event){
-    event.preventDefault();
-
-    var textInput = document.getItemById("textInput").value;
-
-    localStorage.setItem("userInput", userInput);
-})};
-
-
-
 
 // WHEN I refresh the page
 // THEN the saved events persist
 
 
-
-//logic to loop over hours of the day
-
-// for loop to loop over 9-5 (0900-1700)
-//before for loop, get and store current time using moment.js
-// if time > timeblock, grey it out and go next available hour
-
-
-
-// apply appropriate status to an html element helping represent taht hour of the day
-
-// take different inputs the user is giving in different textareas
+})
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # work-day-scheduler
+Homework Week 5
+
+## Table of Contents
+
+1. Installation
+2. Usage
+3. Images
+4. Credits
+5. License
+6. URL 
+
+## Installation
+
+Clone with SSH Key:
+git@github.com:cwf1984/work-day-scheduler.git
+
+Or use the URL listed below to pull up the live. 
+
+## Usage
+
+* The current date displays at the top of the page
+* There are timeblocks for each hour between 9AM and 5PM
+* Depending on the time of the day, times are color coded to match if they are in the past, present or future.
+* Clicking in the textarea next to an hour, the user is able to type input and it is saved in that block and in local storage on their computer. 
+
+## Images
+
+## Credits
+
+As I have said with each preceding homework assignment, I want to thank Bootcamp Spot, the University of Washington and Trilogy Eduction for the assignment and code they provided. This assignment was very challenging, I am not quite there yet but it has been great practice. Thank you to the instructor and TA's for their dedication and patience. I also want to give thanks to my tutor, Zach Auerbach, for his dedication and help.
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Vanessa B]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## URL
+https://cwf1984.github.io/work-day-scheduler/
