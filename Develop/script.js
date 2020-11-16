f4855651541ca7d28cf618c95eb8e75864e65cad
Vanessa Malloy <catwoman.fan.84@gmail.com>
2020-11-15 17:23:42 -0800

    for loop work

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,37 @@
 // GIVEN I am using a daily planner to create a schedule
 
 $(document).ready(function(){
+    
+    var currentDay = moment();
+    $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 
+    var description = $(".description");
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+    var currentHour = parseInt(currentDay.format("H"));
+    console.log (currentHour);
 
-var currentDay = moment();
-$("#currentDay").text(currentDay.format("MMM Do, YYYY"));
+    var scheduleHours = ["9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
+    var timeIndex = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
+    for (i = 0; i < scheduleHours.length; i++) {
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// 
-var dayHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-var description = $(".description");
+        //set .description to timeIndex
+        // timeIndex.addClass("description");
+        
+        
+        if (timeIndex[i] < currentHour){
+            description.addClass("past");
+        }
+    
+        else if (timeIndex[i] === currentHour) {
+            description.addClass("present");
+        }
+    
+        else {
+            description.addClass("future");
+        }
 
-// this is supposed to loop over the current hour, which is listed in dayHours variable to change the color of the block
-for (var i = 0; i < dayHours.length; i++) {
-    var currentHourOne = parseInt(currentDay.format("H"));
-    var currentHourTwo = currentDay.hours();
-    console.log(currentHourOne, currentHourTwo);
-
-    if (currentHourOne < currentDay) {
-    description.attr("class", "past");
     }
-
-    else if (currentHourTwo == currentDay) {
-    description.attr("class", "present");
-    }
-
-    else {
-    description.attr("class", "future");
-    }
-};
 
 
 // WHEN I click into a timeblock
@@ -45,13 +43,13 @@ for (var i = 0; i < dayHours.length; i++) {
 // THEN the saved events persist
 //  I am still working on trying to get local storage to work
 
-$(".saveBtn").on("click", function(){
-    var siblingEl = $(this).siblings("#textInput").val();
-    console.log(clickEvent);
-    var parentEl = $(this).parent().attr("id");
-    localStorage.setItem(clickEvent, parentEl); 
-    localStorage.getItem(JSON.stringify(clickEvent));
-});
+    $(".saveBtn").on("click", function(){
+        var siblingEl = $(this).siblings("#textInput").val();
+        console.log(clickEvent);
+        var parentEl = $(this).parent().attr("id").val();
+        localStorage.setItem(clickEvent, parentEl); 
+    // localStorage.getItem(JSON.stringify(clickEvent));
+    });
 
 
 })
